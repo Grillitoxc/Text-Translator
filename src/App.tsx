23b75de4679c0f4e7c37b4cc3b635b8c6c48822b
Christopher Torres <christopher.torres@usach.cl>
2023-11-23 23:03:48 -0300

    [feat]: styling the boxes of the traductor

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import { useStore } from './hooks/Store'
+import './styles/styles.css'
 
 function App () {
   const { fromLanguage, setFromLanguage } = useStore()
 
   return (
-    <div>
-      <h1>Google Translate</h1>
-      <button onClick={() => { setFromLanguage('es') }}>
-        Cambiar a español
-      </button>
-      {fromLanguage}
+    <div className='container'>
+      <div className='card input-wrapper'>
+        <div className="from-text">
+          <span className="heading">
+            From:</span>
+          {/* dropdown to select the language */}
+          <div className="dropdown-container active" id='input-text'>
+            <div className="dropdown-toggle">
+              <span className="selected">Detección automática</span>
+            </div>
+            <ul className='dropdown-menu'>
+              <li className="option active">Español</li>
+              <li className="option">Inglés</li>
+              <li className="option">Francés</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className='card output-wrapper'></div>
     </div>
   )
 }
