ad9833d832d13768e7edd3d2b111d38f3ec3ffc0
Christopher Torres <christopher.torres@usach.cl>
2023-11-25 23:05:37 -0300

    [feat]: Implements text-to-speech, clipboard and responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import { useStore } from './hooks/Store'
 import './styles/card.css'
 import './styles/swap.css'
+import './styles/scrollbar.css'
+import './styles/header-footer.css'
 import { SUPPORTED_LANGUAGES } from './constants'
 import { type Language, type LanguageOptions } from './types'
 import { useDebounce } from './hooks/Debounce'
@@ -29,27 +31,6 @@ function App () {
     const fetchData = async () => {
       if (debouncedInputText === '') {
         setTranslatedText('' as Language)
-        return
-      }
-
-      if (fromLanguage === 'es-CL' && toLanguage === 'pt-BR') {
-        const macaco: Record<number, string> = {
-          0: 'Cabesiña mano',
-          1: 'Vanina apunta a la cabeza por el amor de Dios',
-          2: 'Las primeras dos rondas no cuentan',
-          3: 'Morador...',
-          4: 'CT CONCHETUMARE CT',
-          5: 'voce e um macaco',
-          6: 'Verde amistad, amistad verde',
-          7: 'Jungla manito, jungla manito',
-          8: 'John Serrano'
-        }
-
-        setTimeout(() => {
-          const random = Math.floor(Math.random() * 9)
-          console.log(random)
-          setTranslatedText(macaco[random] as Language)
-        }, 500)
         return
       }
 
@@ -107,92 +88,124 @@ function App () {
     setInputText(translatedText as Language)
   }
 
+  const githubProject = 'https://github.com/Grillitoxc/Google-Translate-GPT-Api'
+  const githubAuthor = 'https://github.com/Grillitoxc'
+  const openLink = (link: string) => {
+    window.open(link, '_blank')
+  }
+
+  const handleSpeaker = (language: Language, text: string) => {
+    const utterance = new SpeechSynthesisUtterance(text)
+    utterance.lang = language
+    speechSynthesis.getVoices()
+    utterance.rate = 0.9
+    speechSynthesis.speak(utterance)
+  }
+
+  const handleClipboard = (text: string) => {
+    navigator.clipboard.writeText(text).catch((error) => {
+      console.error('No se pudo copiar el texto: ', error)
+    })
+  }
+
   return (
-    <div className="container">
-      <div className="card input-wrapper">
-        <div className="from-text">
-          <span className="heading">Idioma:</span>
-          <div className={`dropdown-container ${inputDropdownOpen ? 'active' : ''}`} id="input-text">
-            <div className="dropdown-toggle" onClick={toggleInputDropdown}>
-              <i className="fa-solid fa-globe"></i>
-              {/* <span className="selected">{inputSelectedLanguage === 'auto' ? 'Detección automática' : inputSelectedLanguage}</span> */ }
-              <span className="selected">{inputSelectedLanguage}</span>
-              <i className={`fa-solid fa-chevron-${inputDropdownOpen ? 'up' : 'down'}`}></i>
+    <>
+      <div className="header">
+        <h1>Traductor de texto</h1>
+      </div>
+      <div className="container">
+        <div className="card input-wrapper">
+          <div className="from-text">
+            <span className="heading">Idioma:</span>
+            <div className={`dropdown-container ${inputDropdownOpen ? 'active' : ''}`} id="input-text">
+              <div className="dropdown-toggle" onClick={toggleInputDropdown}>
+                <i className="fa-solid fa-globe"></i>
+                <span className="selected">{inputSelectedLanguage}</span>
+                <i className={`fa-solid fa-chevron-${inputDropdownOpen ? 'up' : 'down'}`}></i>
+              </div>
+              <ul className="dropdown-menu">
+                {Object.entries(SUPPORTED_LANGUAGES).map(([key, value]) => (
+                  <li
+                    key={key}
+                    className={`option ${inputSelectedLanguage === value ? 'active' : ''}`}
+                    onClick={selectInputLanguage(key as LanguageOptions, value)}
+                  >
+                    {value}
+                  </li>
+                ))}
+              </ul>
             </div>
-            <ul className="dropdown-menu">
-              {/* fromLanguage !== 'auto' && (
-                <li
-                  className={`option ${inputSelectedLanguage === 'auto' ? 'active' : ''}`}
-                  onClick={selectInputLanguage('auto', 'Detección automática')}
-                >
-                  Detección automática
-                </li>
-              ) */}
-              {Object.entries(SUPPORTED_LANGUAGES).map(([key, value]) => (
-                <li
-                  key={key}
-                  className={`option ${inputSelectedLanguage === value ? 'active' : ''}`}
-                  onClick={selectInputLanguage(key as LanguageOptions, value)}
-                >
-                  {value}
-                </li>
-              ))}
-            </ul>
           </div>
+          <div className="text-area">
+            <textarea
+              className="input"
+              placeholder="Escribe algo..."
+              id="input-text"
+              value={inputText}
+              onChange={handleInputChange}
+            ></textarea>
+            <div className="icons-container">
+              <div className="icons">
+                <i className="fa-solid fa-copy" onClick={() => { handleClipboard(inputText) }}></i>
+                <i className="fa-solid fa-volume-high" onClick={() => { handleSpeaker(fromLanguage as Language, inputText) }}></i>
+              </div>
+              <span id="input-chars">{inputText.length} / 300</span>
+            </div>
+          </div>
+
         </div>
-        <div className="text-area">
-          <textarea
-            className="input"
-            placeholder="Escribe algo..."
-            id="input-text"
-            value={inputText}
-            onChange={handleInputChange}
-          ></textarea>
-          <div className="chars">
-            <span id="input-chars">{inputText.length}</span>/300
+        <div className="center" onClick={handleSwitchLanguages}>
+          <div className="swap-position">
+            <i className="fa-solid fa-right-left"></i>
           </div>
         </div>
 
-      </div>
-      <div className="center" onClick={handleSwitchLanguages}>
-        <div className="swap-position">
-          <i className="fa-solid fa-right-left"></i>
-        </div>
-      </div>
-
-      <div className='card output-wrapper'>
-        <div className="from-text">
-          <span className="heading">Idioma:</span>
-          <div className={`dropdown-container ${outputDropdownOpen ? 'active' : ''}`} id="input-text">
-            <div className="dropdown-toggle" onClick={toggleOutputDropdown}>
-              <i className="fa-solid fa-globe"></i>
-              <span className="selected">{outputSelectedLanguage}</span>
-              <i className={`fa-solid fa-chevron-${outputDropdownOpen ? 'up' : 'down'}`}></i>
+        <div className='card output-wrapper'>
+          <div className="from-text">
+            <span className="heading">Idioma:</span>
+            <div className={`dropdown-container ${outputDropdownOpen ? 'active' : ''}`} id="input-text">
+              <div className="dropdown-toggle" onClick={toggleOutputDropdown}>
+                <i className="fa-solid fa-globe"></i>
+                <span className="selected">{outputSelectedLanguage}</span>
+                <i className={`fa-solid fa-chevron-${outputDropdownOpen ? 'up' : 'down'}`}></i>
+              </div>
+              <ul className="dropdown-menu">
+                {Object.entries(SUPPORTED_LANGUAGES).map(([key, value]) => (
+                  <li
+                    key={key}
+                    className={`option ${outputSelectedLanguage === value ? 'active' : ''}`}
+                    onClick={selectOutputLanguage(key as Language, value)}
+                  >
+                    {value}
+                  </li>
+                ))}
+              </ul>
             </div>
-            <ul className="dropdown-menu">
-              {Object.entries(SUPPORTED_LANGUAGES).map(([key, value]) => (
-                <li
-                  key={key}
-                  className={`option ${outputSelectedLanguage === value ? 'active' : ''}`}
-                  onClick={selectOutputLanguage(key as Language, value)}
-                >
-                  {value}
-                </li>
-              ))}
-            </ul>
+          </div>
+          <div className="text-area">
+            <textarea
+              className="output"
+              disabled
+              placeholder={loading ? 'Traduciendo...' : 'Traducción'}
+              id="output-text"
+              value={translatedText}
+            ></textarea>
+            <div className="icons">
+              <i className="fa-solid fa-copy" onClick={() => { handleClipboard(translatedText) }}></i>
+              <i className="fa-solid fa-volume-high" onClick={() => { handleSpeaker(toLanguage as Language, translatedText) }}></i>
+            </div>
           </div>
         </div>
-        <div className="text-area">
-          <textarea
-            className="output"
-            disabled
-            placeholder={loading ? 'Traduciendo...' : 'Traducción'}
-            id="output-text"
-            value={translatedText}
-          ></textarea>
-        </div>
       </div>
-    </div>
+      <div className="footer">
+        <i className="fa-brands fa-github" onClick={() => { openLink(githubProject) }}></i>
+        <p
+          className='footer-text'
+          onClick={() => { openLink(githubAuthor) }}>
+          Hecho por <span className='github-author'>Grillitoxc</span>
+        </p>
+      </div>
+    </>
   )
 }
 
